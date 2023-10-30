72dad04a17f2d8985d5aa1894cf8793dcc55a271
Kavin M <89208380+kavinm21@users.noreply.github.com>
2023-10-30 10:59:25 +0530

    running tests

--- api-test.py
+++ api-test.py
@@ -6,3 +6,14 @@ load_dotenv()
 public_key = os.getenv('PUBLIC_KEY')
 private_key = os.getenv('PRIVATE_KEY')
 
+from marvel import Marvel
+
+m = Marvel(public_key, private_key)
+
+comics = m.comics
+
+data = comics.all(titleStartsWith='Daredevil',dateRange="2020-01-01,2021-01-01", limit="25")
+
+df = data['data']['results']
+print(data['data']['count'])
+print(df[0])
